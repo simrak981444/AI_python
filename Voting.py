ec3be05609cf9469f40c81cb55cfd1b763985cf8
Simranjeet <simranjeet981444>
2025-03-18 15:22:33 +0200

    ok

--- Voting.py
+++ Voting.py
@@ -1,10 +1,7 @@
-
 age = int(input("Please enter your age: "))
 nationality = input("Are you a citizen? (yes/no): ").lower()
-
-
 if age < 18:
-    print("You are not eligible for voting.")
+    print("Wait until you are of 18.")
 elif nationality == "yes" and age >= 18:
     print("Congratulations! You have right to vote.")
 else:
